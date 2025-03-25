dd790419f85a2db26ae4c4923c1af5a8becdee3c
Omar Badr <obadr7820@sdsu.edu>
2025-03-25 12:00:14 -0700

    change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    base: '/my-portfolio/',
+    base: '/',
     publicDir: 'public',
     define: {
       'process.env': env
